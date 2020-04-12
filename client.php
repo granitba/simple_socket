f5577752de495a3793521d09769149daed2841e0
granitba <granittiti@gmail.com>
2020-04-12 16:20:30 +0200

    Client.php accepts command line arguments, add comments to serv.php, remove bad.php and added readme.md

--- client.php
+++ client.php
@@ -1,18 +1,18 @@
 <?php
 $address="127.0.0.1";
-$port="467";
+$port="3333";
 
-if ($argv[1] !== '') {
+if ($argv[1] !== '') {      // If there is a command line argument set the message to that
     $msg = $argv[1];
 } else {
-    $msg="Hello";
+    $msg="Hello";           // If not default to "Hello"
 }
 
 
-$sock=socket_create(AF_INET,SOCK_STREAM,0) or die("Cannot create a socket");
-socket_connect($sock,$address,$port) or die("Could not connect to the socket");
-socket_write($sock,$msg);
+$sock=socket_create(AF_INET,SOCK_STREAM,0) or die("Cannot create a socket"); // Open a tcp socket
+socket_connect($sock,$address,$port) or die("Could not connect to the socket");                   // Connect to socket opened on serv.php
 
-$read=socket_read($sock,1024);
+socket_write($sock,$msg);               //Write our message to the socket
+$read=socket_read($sock,1024);   // Read the response
 echo $read;
 socket_close($sock);
--- serv.php
+++ serv.php
@@ -1,30 +1,41 @@
 <?php
-$i = 1;         //Counter variable
+$i = 1;         // Counter variable
 $run = true;
 $address = "0.0.0.0";
-$port = "467";
+$port = "3333";
 $sock = socket_create(AF_INET, SOCK_STREAM, 0) or die("Can't create socket");
 
+// Bind to the socket that we created and listen to it for connections
 socket_bind($sock, $address, $port) or die("Couldn't bind to socket");
 socket_listen($sock) or die("Couldn't listen to socket");
 
 
  while ($run) {
 
+     // Accept any incoming connection
      $accept = socket_accept($sock) or die("Couldn't accept socket");
+
+     // We use php_binary_read because normal read behaves very strange with newlines
+     // and it's also blocking regardless of socket_non_block
      $read = socket_read($accept,1024, PHP_BINARY_READ) or die("Cannot read from socket");
 
-     str_replace(array("\n\r", "\n", "\r", PHP_EOL), '', $read);            //Replace newline to avoid conflicts
-     $read = preg_replace('/[\x00-\x1F\x7F]/u', '', $read);      //Remove unicode chars
+     // Replace all newline characters with empty space, usually only looking for PHP_EOL would suffice,
+     // but not when sending data from windows to unix or vice versa
+     str_replace(array("\n\r", "\n", "\r", PHP_EOL), '', $read);
+
+     // Replace obscure UTF characters with empty space (without this my string wouldn't compare correctly)
+     $read = preg_replace('/[\x00-\x1F\x7F]/u', '', $read);
      echo $read . PHP_EOL;
 
-     if (strcmp($read, "Mixbot:egoniss432") == 0) {
-         socket_write($accept,"egonissi c.g." . PHP_EOL);
+     // Strcmp will return 0 if the strings match
+     if (strcmp($read, "Target") == 0) {
+         $i = 1;
+         socket_write($accept,"Reply" . PHP_EOL);
      }
-     else if (strcmp($read,"hello version 352 user:egoniss432") == 0) {
+     else if (strcmp($read,"Hello target") == 0) {
          socket_write($accept, "1" . PHP_EOL);
      }
-     else if (strcmp($read,"keepgoing egoniss432") == 0) {
+     else if (strcmp($read,"keepgoing") == 0) {
          socket_write($accept, ++$i . PHP_EOL);
      } else {
          $run = false;
